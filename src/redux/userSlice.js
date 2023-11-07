76a5d39b48cd8907b40a15c59addd44cd7f74c73
Kazım Etiksan <kazim@pikselmutfak.com>
2023-11-07 13:39:25 +0300

    userSlice

---
+++ src/redux/userSlice.js
@@ -0,0 +1,17 @@
+import {
+    createSlice
+} from '@reduxjs/toolkit'
+
+const initialState = [{
+    firstName: "Hakan",
+    lastName: "Şahin",
+    age: "34"
+}]
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {}
+})
+
+export default userSlice.reducer // ?
